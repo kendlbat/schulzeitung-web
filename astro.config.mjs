fdba4ac16bd9017d2e9ab516c7b7af9e9e8b6826
Tobias Kendlbacher <kendlbat@edu.htl-villach.at>
2023-09-22 18:40:04 +0200

    Fix obscure top-level-await build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
-    // site: 'https://example.com',
+    site: 'https://example.com',
     integrations: [mdx(), sitemap()],
-    output: "server",
-    adapter: cloudflare(),
-    build: {
-        target: 'esnext' //browsers can handle the latest ES features
+    vite: {
+        build: {
+            minify: false,
+        }
     }
 });
