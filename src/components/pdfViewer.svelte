b5076af4d9e547ba485fa6e54f9e5c84052b8636
Tobias Kendlbacher <kendlbat@edu.htl-villach.at>
2023-11-19 23:36:31 +0100

    Fix bug

--- src/components/pdfViewer.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    export let src: string;
-</script>
-
-<iframe
-    src={`/pdfjs-dist/web/viewer.html?file=${src}`}
-    title="PDF Viewer"
-    frameborder="0"
-    class="h-full w-full"></iframe>
