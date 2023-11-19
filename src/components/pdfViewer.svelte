108a31a69a53787c14d0ca7500537d133af28cdf
Tobias Kendlbacher <kendlbat@edu.htl-villach.at>
2023-11-19 23:13:39 +0100

    Cross-platform pdf viewer

---
+++ src/components/pdfViewer.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let src: string;
+</script>
+
+<iframe
+    src={`/pdfjs-dist/web/viewer.html?file=${src}`}
+    title="PDF Viewer"
+    frameborder="0"
+    class="h-full w-full"></iframe>
